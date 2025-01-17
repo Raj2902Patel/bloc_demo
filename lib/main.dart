39eafc161e7c954a8cbab23414eaa6c8dc72d430
Raj2902Patel <rajpatel.imscit20@gmail.com>
2025-01-17 12:45:53 +0530

    Folder Structure Changed!

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:bloc_demo/bloc/calculator/calculatorCubit.dart';
 import 'package:bloc_demo/bloc/cubit/counterCubit.dart';
 import 'package:bloc_demo/bloc/equatable/counterBloc.dart';
-import 'package:bloc_demo/pages/counterPage.dart';
+import 'package:bloc_demo/bloc/equatable/counterPage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
--- lib/pages/counterCubit.dart
+++ lib/pages/counterCubit.dart
@@ -1,5 +1,5 @@
+import 'package:bloc_demo/bloc/calculator/calculatorPage.dart';
 import 'package:bloc_demo/bloc/cubit/counterCubit.dart';
-import 'package:bloc_demo/pages/calculatorPage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
